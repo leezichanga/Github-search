420fc14a49f9272f0d9058fe1f8f994946032483
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-03-25 14:57:41 +0300

    Add about component Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { GithubPageService } from './githubpage/github.service';
 import { FormsModule } from '@angular/forms';
 import { StrikethroughDirective } from './strikethrough.directive';
+import { AboutComponent } from './about/about.component';
+
+// Defining routes
+const routes:Routes=[
+  {path:"github",component:GithubComponent},
+  {path:"about",component:AboutComponent},
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    StrikethroughDirective
+    StrikethroughDirective,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [GithubPageService],
   bootstrap: [AppComponent]

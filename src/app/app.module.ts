99aa30b9faf65ffda211b1bce859c48944a0a14c
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-03-25 15:16:34 +0300

    Add 404 pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
 import {RouterModule, Routes} from '@angular/router';
+// import {commonModule} from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
@@ -9,23 +10,28 @@ import { GithubPageService } from './githubpage/github.service';
 import { FormsModule } from '@angular/forms';
 import { StrikethroughDirective } from './strikethrough.directive';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 // Defining routes
 const routes:Routes=[
   {path:"github",component:GithubComponent},
   {path:"about",component:AboutComponent},
+ {path:"",redirectTo:"/github",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
     StrikethroughDirective,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    // CommonModule,
     RouterModule.forRoot(routes),
   ],
   providers: [GithubPageService],
